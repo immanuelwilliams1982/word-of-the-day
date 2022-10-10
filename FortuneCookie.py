ebdc500b3de793048feac4b9e7d144ad85d022fe
immanuelwilliams1982 <114463777+immanuelwilliams1982@users.noreply.github.com>
2022-10-10 21:53:39 +0530

    crystal ball fortune cookie
    a fortune cookie or crystal ball that shows a message when run

---
+++ FortuneCookie.py
@@ -0,0 +1,53 @@
+#This is a crystal ball program which generates a fortune
+import random
+wordArray=["abandon, let-go, forsake, leave it behind",
+           "abide in the infinite, dwell there in peace",
+           "welcome aboard, you are at the right place",
+           "absorb, accept, imbibe"
+           "You may have to stop something, abolish, prevent",
+           "absurdity and irony adds fun",
+           "abundance knows no limits",
+           "kindness is seeing with compassion",
+           "seek accuracy, seek facts and truth",
+           "acknowledge the presence",
+           "avoid addiction, dependency, value independece",
+           "Having enough or feeling satisfied is a state of being",
+           "adoloscence is a period of transition",
+           "alter or change something, make it different",
+           "developing skills vs pastime",
+           "a pleasant surprise",
+           "think about the next festival",
+           "waiting with patience",
+           "an earnest request, a prayer",
+           "ask for and take help when needed",
+           "take it forward, proceed",
+           "this is exactly what is needed",
+           "architect foresees the structures of future",
+           "bringing together people and resources",
+           "what you think about, you may hold in your hands",
+           "talking your mind is natural",
+           "being assertive is sometimes necessary",
+           "satsang keeping good thoughts and good company",
+           "stay away from bad company and bad situations",
+           "awareness, knowing, understanding, realization",
+           "awesome always inspires wonder",
+           "Being graceful does matter",
+           "Even bachelors are married to something",
+           "Life is a mystery to be lived",
+           "Karma is that baggage with your name",
+           "Ban the baits in your life",
+           "You are needed",
+           "Being generous is beneficial",
+           "Be loyal to a cause you value the most",
+           "Make compassionate choices and decisions",
+           "You have a story to write, a song to sing",
+           "Misktakes are lessons learnt"
+           ]
+
+w=wordArray[random.randrange(0,41)]
+
+print(w)
+
+
+
+
